decb47e436e842e73ab65dcfa0d2a4e15a31e005
Kamran <kamrantahir25@gmail.com>
2019-11-08 21:57:49 +0530

    :wheelchair: Refragmenting the code.
    Restructring whole codebase.

--- index.js
+++ index.js
@@ -1,82 +1,34 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const {prefix, TOKEN} = require('./config.json');
-const chalk = require('chalk');
+const {Client} = require('discord.js');
+const {config} = require('dotenv');
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
-const commandFiles = fs
-	.readdirSync('./commands')
-	.filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
-
-const cooldowns = new Discord.Collection();
-
-client.once('ready', () => {
-	console.log(chalk.blue('Ready!'));
+// Declares our bot,
+// the disableEveryone prevents the client to ping @everyone
+const client = new Client({
+	disableEveryone: true
 });
 
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
-	const command =
-		client.commands.get(commandName) ||
-		client.commands.find(
-			cmd => cmd.aliases && cmd.aliases.includes(commandName)
-		);
-	console.info(chalk.green(`Called command: ${commandName}`));
-	console.info(chalk.magentaBright(`User- ${message.author.tag}`));
-	console.info(chalk.white('-------------------'));
-
-	if (!command) return;
-
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-
-		return message.channel.send(reply);
-	}
-
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 60) * 1000;
-
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(
-				`please wait ${timeLeft.toFixed(
-					1
-				)} more second(s) before reusing the \`${command.name}\` command.`
-			);
-		}
-	}
-
-	timestamps.set(message.author.id, now);
-	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-	try {
-		command.execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+config({
+	path: __dirname + '/.env'
 });
 
-client.login(TOKEN);
+// When the bot's online, what's in these brackets will be executed
+client.on('ready', () => {
+	console.log(`Hi, ${client.user.username} is now online!`);
+
+	// Set the user presence
+	client.user.setPresence({
+		status: 'online',
+		game: {
+			name: 'me getting developed',
+			type: 'WATCHING'
+		}
+	});
+});
+
+// When a message comes in, what's in these brackets will be executed
+client.on('message', async message => {
+	console.log(`${message.author.username} said: ${message.content}`);
+});
+
+// Login the bot
+client.login(process.env.TOKEN);
